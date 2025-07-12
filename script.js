371817a3a47ccf205fe42aa93d66b8cd321bbd3c
Md Rayhan ul Fardous <49313477+rayhan-fardous@users.noreply.github.com>
2025-07-12 22:57:59 +0600

    adding save favorites

--- script.js
+++ script.js
@@ -10,6 +10,7 @@ const apiKey = "DEMO_KEY";
 const apiUrl = `https://api.nasa.gov/planetary/apod?api_key=${apiKey}&count=${count}`;
 
 let resultArray = [];
+let favorites ={};
 
 function updateDOM() {
   resultArray.forEach((result) => {
@@ -37,6 +38,8 @@ function updateDOM() {
     // Save Text
     const saveText = document.createElement("p");
     saveText.classList.add("clickable");
+    saveText.textContent = 'Add to Favorites';
+    saveText.setAttribute('onclick', `savefavorite('${result.url}')`); 
     // Card Text
     const cardText = document.createElement("p");
     cardText.textContent = result.explanation;
@@ -71,6 +74,13 @@ async function getNasaPictures() {
     // Catch Error Here
   }
 }
+
+// Add result to Favorites
+function savefavorite(itemUrl) {
+  // Loop through Results Array to select Favorite
+  
+}
+
 
 // On Load
 getNasaPictures();
